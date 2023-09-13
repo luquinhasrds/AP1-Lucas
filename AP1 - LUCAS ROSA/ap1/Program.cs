eeb52c8a72448d4f562c2c5a86c3cc01f072c661
luquinhasrds <101369107+luquinhasrds@users.noreply.github.com>
2023-09-13 20:20:11 -0300

    Add files via upload

---
+++ AP1 - LUCAS ROSA/ap1/Program.cs
@@ -0,0 +1,61 @@
+﻿do{ //SISTEMA DE REPETIÇÃO
+
+    //PEDE O NOME
+    Console.WriteLine("Digite o nome do aluno:");
+    string nome = Console.ReadLine();
+
+    //PEDE A IDADE
+    Console.WriteLine("Digite a idade: ");
+    double idade = Convert.ToDouble(Console.ReadLine());
+
+    //PEDE A ALTURA
+    Console.WriteLine("Digite a altura: (0,00)");
+    double altura = Convert.ToDouble(Console.ReadLine());
+
+    //PEDE O PESO
+    Console.WriteLine("Digite o peso: (00)");
+    double peso = Convert.ToDouble(Console.ReadLine());
+
+    //CALCULA O IMC 
+    double imc = peso / (altura*altura);
+
+    //VE EM QUAL CATEGORIA O IMC CALCULADO SE ENCAIXA
+    string categoria_imc;
+    if(imc <= 18){
+        categoria_imc = "baixo peso";
+    }else if(imc <= 24){
+        categoria_imc = "peso normal";
+    }else if(imc <= 29){
+        categoria_imc = "sobrepeso";
+    }else if(imc <= 34){
+        categoria_imc = "obeso";
+    }else{
+        categoria_imc = "erro de digitação";
+    }
+
+    //VE A IDADE DA PESSOA E VE EM QUAL CATEGORIA SE ENCAIXA
+    string faixa_etaria;
+    if(idade <= 0){
+        faixa_etaria = "IMPOSSIVEL";
+    }else if(idade <= 12){
+        faixa_etaria = "Criança";
+    }else if(idade <= 18){
+        faixa_etaria = "Adolecente";
+    }else if(idade <= 59){
+        faixa_etaria = "Adulto";
+    }else if(idade >= 60){
+        faixa_etaria = "Idoso";
+    }else{
+        faixa_etaria = "ai é complicado";
+    }
+    
+    Console.WriteLine($"{nome}, {idade}, {imc}, {faixa_etaria}, {categoria_imc}");
+    //PERGUNTA AO USUARIO SE QUER REPETIR O CODIGO OU CONTINUAR
+    Console.WriteLine("Quer calcular IMC de outra pessoa? (s/n)");
+    string opcao = Console.ReadLine();
+    if (opcao != "s"){
+        Console.WriteLine("O programa sera fechado..............");
+        break;
+    }
+
+}while(true); //SISTEMA DE REPETIÇÃO
